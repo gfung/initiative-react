62b87daaac68cfac77cd439ff46ef4be92fe6366
AiyahLappy <gycf84@yahoo.com>
2019-09-08 15:12:30 -0700

    Finished add/sort logic

--- src/orderedlist.js
+++ src/orderedlist.js
@@ -4,7 +4,10 @@ function List(props) {
   return (
     <li id={props.id}>
       <div>
-        {props.name}
+        <span>{props.name}</span>
+        <span> - </span>
+        <span>{props.number}</span>
+        <span> - </span>
         <button type="button" onClick={props.onClick}>X</button>
       </div>
     </li>
@@ -66,35 +69,66 @@ class OrderedList extends Component {
 
   add(){
     let name = document.getElementById('name').value;
-    let number = parseInt(document.getElementById('number').value);
-    let tempid = this.state.id+1;
+    let number = parseInt(document.getElementById('number').value, 10);
     
-    this.setState({
-      anArray: this.state.anArray.concat({name:name, number:number, id: this.state.id}),
-      
-    })
-    this.setState({
-      id: tempid
-    })
+    let tempid = this.state.id+1;
+    this.setState({id: tempid})
+
+    let tempArr = this.state.anArray;
+    let tempLength = tempArr.length
+    //define who should be at top
+    let current = tempArr[0]
+    //insert according to current order
+    let position = 0;
+    for (let i = 0;i < tempLength; i++) {
+      if (number < tempArr[i].number) {
+        position++
+      }
+    }
+    // sort first
+    this.sort()
+    // do the insert
+    this.state.anArray.splice(position,0,{name:name, number:number, id: this.state.id});
+    // get original order of array
+    let newArr = this.state.anArray;
+    
+    let newLength = newArr.length;
+    if (current !== undefined){
+      for (let i = 0;i < newLength;i++) {
+        if (newArr[0].number !== current.number) {
+          this.next()
+        }
+      }
+    }
   }
 
   render() {
     let eleString=[];
     let tempNum = 0;
     for (let index = 0; index < this.state.anArray.length; index++) {
-      eleString.push(<List key={tempNum} id={this.state.anArray[index].id} name={this.state.anArray[index].name} onClick={this.remove.bind(this,this.state.anArray[index].id)} />)
+      eleString.push(<List key={tempNum} id={this.state.anArray[index].id} name={this.state.anArray[index].name} number={this.state.anArray[index].number} onClick={this.remove.bind(this,this.state.anArray[index].id)} />)
       tempNum++
     }
     return (
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="string" name="name" />
-        <label htmlFor="number">Number</label>
-        <input id="number" type="number" name="number" />
-        <button type="button" onClick={this.add} >Add</button>
-        <button type="button" onClick={this.next} >Next</button>
-        <button type="button" onClick={this.sort} >Sort</button>
-        <ol>{eleString}</ol>
+      <div class='grid-container'>
+        
+        <div style={{'text-align':'center'}}>D20 Initiative</div>
+        <br></br>
+        <div style={{'text-align':'center'}}>
+          <label htmlFor="name">Name</label>
+          <input id="name" type="string" name="name" />
+          <label htmlFor="number">Number</label>
+          <input id="number" type="number" name="number" />
+          <button type="button" onClick={this.add} >Add</button>
+          <button type="button" onClick={this.sort} >Sort</button>
+        </div>
+        <br></br>
+        <div style={{'text-align':'center'}}>
+          <button type="button" onClick={this.next} >Next</button>
+        </div>
+        <br></br>
+        <div style={{'text-align':'center'}}>Current Turn : {this.state.anArray[0] ? this.state.anArray[0].name:""}</div>
+        <ul style={{'text-align':'center', 'list-style': 'none'}}>{eleString}</ul>
       </div>
     )  
   }
